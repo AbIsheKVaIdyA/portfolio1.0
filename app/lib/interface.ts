87e9df01a79634f3f0c463249d509719bbd08ae1
Abhishek Vaidya <abhishekvaidya.work@gmail.com>
2024-05-26 18:58:04 +0530

    second commit

---
+++ app/lib/interface.ts
@@ -0,0 +1,8 @@
+export interface ProjectsCard {
+    title: string;
+    _id: string;
+    link: string;
+    description: string;
+    tags: string[];
+    imageUrl: string;
+  }
---
+++ components/Form.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Loader2 } from "lucide-react";
+import { postData } from "@/app/actions";
+import { useRef } from "react";
+
+export function Form() {
+  const formRef = useRef<HTMLFormElement>(null);
+  return (
+    <form
+      ref={formRef}
+      action={async (FormData) => {
+        await postData(FormData);
+        formRef.current?.reset();
+      }}
+      className="flex justify-between gap-4 flex-col md:flex-row"
+    >
+      <Input
+        type="text"
+        name="message"
+        maxLength={80}
+        minLength={1}
+        placeholder="Your Message..."
+        required
+      />
+      <SubmitButton />
+    </form>
+  );
+}
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <>
+      {pending ? (
+        <Button disabled>
+          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+          Please Wait
+        </Button>
+      ) : (
+        <Button type="submit">Sign for free</Button>
+      )}
+    </>
+  );
+}
